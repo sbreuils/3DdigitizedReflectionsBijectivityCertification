763e308e6b3b3971547b5c6fde2fb2a29a75b244
sbreuils <breuils@nii.ac.jp>
2021-04-26 14:53:12 +0900

    Finished first SNF imp

--- include/linearAlgebra.h
+++ include/linearAlgebra.h
@@ -2,7 +2,14 @@
 
 #pragma once
 
+#include <utility>
+
 namespace gadg{
+
+    typedef std::pair<int,int> MatrixIndex;
+
+
+
 
     // Compute system A
     Eigen::MatrixXi systemA(const gadg::QuaternionScalar<int>& q){
@@ -20,14 +27,147 @@ namespace gadg{
         return A;
     }
 
+
+    MatrixIndex minAij(const Eigen::MatrixXi& A, const int s){
+        int cols = A.cols();
+        int rows = A.rows();
+
+        // init min value with abs
+        Eigen::MatrixXi Aabs = A.block(s,s,rows-s,cols-s).cwiseAbs();
+
+//        std::cout << "Minaij::Aabs = \n"<<Aabs<<std::endl;
+        MatrixIndex AijMin = std::make_pair(s,s);
+        int valMin = Aabs.maxCoeff();
+
+        for(int i=s ; i<rows ;++i){
+            for(int j=s ; j<cols ; ++j){
+                if(Aabs(i-s,j-s)!=0 && Aabs(i-s,j-s) <= valMin){
+                    AijMin = std::make_pair(i,j);
+                    valMin = Aabs(i-s,j-s);
+                }
+            }
+        }
+
+
+
+        return AijMin;
+    }
+
+
+    Eigen::MatrixXi Identity(const int n){
+        return Eigen::MatrixXi::Identity(n,n);
+    }
+
+    void swapRows(Eigen::MatrixXi& A, const int i, const int j){
+        A.row(i).swap(A.row(j));
+    }
+
+    void swapColumns(Eigen::MatrixXi& A, const int i, const int j){
+        A.col(i).swap(A.col(j));
+    }
+
+    void addToRow(Eigen::MatrixXi& A,const int i, const int factor, const int j) {
+        A.row(i) += factor*A.row(j);
+    }
+
+    void addToColumn(Eigen::MatrixXi& A,const int i, const int factor, const int j) {
+        A.col(i) += factor*A.col(j);
+    }
+
+    void changeSignRow(Eigen::MatrixXi& A,const int i){
+        A.row(i) *=-1;
+    }
+
+    void changeSignColumn(Eigen::MatrixXi& A,const int i){
+        A.col(i) *=-1;
+    }
+
+    // simply check that the entry at place (s,s) in A is the only non-zero in its
+    //s-th column under s and s-th row at the right of s
+    bool isLone(const Eigen::MatrixXi& A,const int s) {
+        for(int j=s+1 ;j<A.cols();++j){
+            if(A(s,j)!=0)
+                return false;
+        }
+
+        for(int i=s+1 ;i<A.rows();++i){
+            if(A(i,s)!=0)
+                return false;
+        }
+        return true;
+    }
+
+    // find an element which is not divisible by M(s,s)
+    MatrixIndex getNextEntry(const Eigen::MatrixXi& A, const int s){
+        for(int i=s+1;i<A.rows();++i){
+            for(int j=s+1;j<A.cols();++j){
+                if(A(i,j)%A(s,s) !=0)
+                    return std::make_pair(i,j);
+            }
+        }
+        return std::make_pair(-1,-1); // nothing found
+    }
+
+
+
     // compute the Hermite normal form of A with an equivalent algo.
-    Eigen::MatrixXi HermiteNormalForm(Eigen::MatrixXi A){
-        // hermite normal form of 
-        Eigen::MatrixXi H
-        Eigen::FullPivLU<Eigen::MatrixXd> lu(A.cast<double>());
-        std::cout << "Here is, up to permutations, its LU decomposition matrix:"
-             << std::endl << lu.matrixLU() << std::endl;
-        return
+    // Smith Normal Form S of the M x N matrix A,
+
+    void HermiteNormalForm(const Eigen::MatrixXi& A){
+        // init
+        Eigen::MatrixXi L= Identity(A.rows());
+        Eigen::MatrixXi R= Identity(A.cols());
+
+        Eigen::MatrixXi M = A;
+
+        const int maxs = std::min(M.rows(),M.cols());
+
+        for(int s=0 ; s<maxs ; ++s){
+            std::cout <<"step "<<s+1 <<"/"<<maxs <<std::endl;
+            std::cout << "M = \n"<<M <<"\n" << std::endl;
+
+            while(!(isLone(M,s))){
+                MatrixIndex Mminij =minAij(M,s);
+                swapRows(M,s,Mminij.first);
+                swapRows(L,s,Mminij.first);
+                swapColumns(M,s,Mminij.second);
+                swapColumns(R,s,Mminij.second);
+
+                // add for lines
+                for(int x=s+1 ; x<M.rows() ; ++x){
+                    if(M(x,s) != 0){
+                        int factor = M(x,s) / M(s,s);
+                        addToRow(M,x,-factor,s);
+                        addToRow(L,x,-factor,s);
+                    }
+                }
+
+                // add for columns
+                for(int x=s+1 ; x<M.cols() ; ++x){
+                    if(M(s,x) != 0){
+                        int factor = M(s,x) / M(s,s);
+                        addToColumn(M,x,-factor,s);
+                        addToColumn(R,x,-factor,s);
+                    }
+                }
+
+                if(isLone(M,s)){
+                    MatrixIndex indexEntry = getNextEntry(M,s);
+                    if(indexEntry.first >= 0){ // /todo check
+                        addToRow(M,s,1,indexEntry.first);
+                        addToRow(L,s,1,indexEntry.first);
+                    }else{
+                        if(M(s,s)<0){
+                            changeSignRow(M,s);
+                            changeSignRow(L,s);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
     }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 
 
 
-
+#define TESTED 1
 
 
 
@@ -35,12 +35,42 @@ int main(int argc, char * argv[]){
     std::cout << "A="<<A<<std::endl;
 
     // compute the resolution of the system through the hermite normal form of A
-    gadg::HermiteNormalForm(A);
+    Eigen::MatrixXi Aa(3,4);
+    Aa << 2,3,4 ,8,
+         -6,6,0 ,0,
+         10,0,16,2;
+    std::cout <<"Identity of size 4,4 = \n"<<gadg::Identity(4)<<std::endl;
 
-    std::cout << "gcd of q = " << gadg::gcd((int)q.value.w,(int)q.value.x,(int)q.value.y,(int)q.value.z)<<std::endl;
-    std::cout <<"reduced quaternion = "<<std::endl;
-    q = q/gadg::gcd((int)q.value.w,(int)q.value.x,(int)q.value.y,(int)q.value.z);
-    q.Print();
+    std::cout << "Aa = "<<Aa << std::endl;
+
+//    std::cout << "implementing SNF functions ..."<<std::endl;
+//
+    gadg::MatrixIndex Aijmin = gadg::minAij(Aa,2);
+    std::cout << "After function, Aa =\n"<<Aa <<std::endl;
+    std::cout << "After function, ij min = ("<<Aijmin.first <<","<<Aijmin.second<<")" <<std::endl;
+
+
+//    std::cout <<"test swap of cols and rows ..."<<std::endl;
+//
+//    gadg::swapColumns(Aa,0,1); // Ok
+//    gadg::swapRows(Aa,1,2);// Ok
+//
+//    gadg::changeSignRow(Aa, 1);
+//    gadg::addToRow(Aa,1,4,0);
+//    gadg::addToColumn(Aa,0,-2,1);
+
+
+    std::cout << "matrix Aa after swap =\n"<<Aa<<"\n"<<std::endl;
+
+    bool isLone = gadg::isLone(Aa,0);
+
+    std::cout <<"is lone = "<<isLone<<std::endl;
+
+//
+//    std::cout << "gcd of q = " << gadg::gcd((int)q.value.w,(int)q.value.x,(int)q.value.y,(int)q.value.z)<<std::endl;
+//    std::cout <<"reduced quaternion = "<<std::endl;
+//    q = q/gadg::gcd((int)q.value.w,(int)q.value.x,(int)q.value.y,(int)q.value.z);
+//    q.Print();
 
 
 	return 0;
