02b8caa5e47b0ab6eddd6657b401bcc8c170f56a
sbreuils <breuils@nii.ac.jp>
2021-04-27 06:23:37 +0900

    Start computing integer set of points inside a region

--- include/digitalGeometry.h
+++ include/digitalGeometry.h
@@ -1,36 +1,45 @@
 #pragma once
 
+namespace gadg {
+
 // compute the origin cubid voxel C[0]
-gadg::Region CZero_Region() {
-    return std::make_tuple(
-            gadg::QuaternionScalar<float>(-0.5,-0.5,-0.5,0.0),gadg::QuaternionScalar<float>(-0.5,0.5,-0.5,0.0),
-            gadg::QuaternionScalar<float>(0.5,-0.5,-0.5,0.0),gadg::QuaternionScalar<float>(0.5,0.5,-0.5,0.0),
-            gadg::QuaternionScalar<float>(-0.5,-0.5,0.5,0.0),gadg::QuaternionScalar<float>(-0.5,0.5,0.5,0.0),
-            gadg::QuaternionScalar<float>(0.5,-0.5,0.5,0.0),gadg::QuaternionScalar<float>(0.5,0.5,0.5,0.0)
-    );
-}
+    Region CZero_Region() {
+        return std::make_tuple(
+                gadg::QuaternionScalar<float>(-0.5, -0.5, -0.5, 0.0),
+                gadg::QuaternionScalar<float>(-0.5, 0.5, -0.5, 0.0),
+                gadg::QuaternionScalar<float>(0.5, -0.5, -0.5, 0.0), gadg::QuaternionScalar<float>(0.5, 0.5, -0.5, 0.0),
+                gadg::QuaternionScalar<float>(-0.5, -0.5, 0.5, 0.0), gadg::QuaternionScalar<float>(-0.5, 0.5, 0.5, 0.0),
+                gadg::QuaternionScalar<float>(0.5, -0.5, 0.5, 0.0), gadg::QuaternionScalar<float>(0.5, 0.5, 0.5, 0.0)
+        );
+    }
 
 
 // compute the transformation of a region
-gadg::Region QuaternionRegionMultiplication(gadg::Region cell, gadg::QuaternionScalar<int>& c) {
-    //
-    gadg::QuaternionScalar<float> c_cast((float)c.value.x,(float)c.value.y,(float)c.value.z,(float)c.value.w);
-    return std::make_tuple(
-            std::get<0>(cell)*c_cast,std::get<1>(cell)*c_cast,
-            std::get<2>(cell)*c_cast,std::get<3>(cell)*c_cast,
-            std::get<4>(cell)*c_cast,std::get<5>(cell)*c_cast,
-            std::get<6>(cell)*c_cast,std::get<7>(cell)*c_cast
-    );
-}
+    Region QuaternionRegionMultiplication(gadg::Region cell, gadg::QuaternionScalar<int> &c) {
+        //
+        gadg::QuaternionScalar<float> c_cast((float) c.value.x, (float) c.value.y, (float) c.value.z,
+                                             (float) c.value.w);
+        return std::make_tuple(
+                std::get<0>(cell) * c_cast, std::get<1>(cell) * c_cast,
+                std::get<2>(cell) * c_cast, std::get<3>(cell) * c_cast,
+                std::get<4>(cell) * c_cast, std::get<5>(cell) * c_cast,
+                std::get<6>(cell) * c_cast, std::get<7>(cell) * c_cast
+        );
+    }
 
 // compute the transformation of a region
-gadg::IntegerRegion regionRounding(gadg::Region cell) {
-    return std::make_tuple(
-            gadg::toLipschitzQuaternion(std::get<0>(cell)),gadg::toLipschitzQuaternion(std::get<1>(cell)),
-            gadg::toLipschitzQuaternion(std::get<2>(cell)),gadg::toLipschitzQuaternion(std::get<3>(cell)),
-            gadg::toLipschitzQuaternion(std::get<4>(cell)),gadg::toLipschitzQuaternion(std::get<5>(cell)),
-            gadg::toLipschitzQuaternion(std::get<6>(cell)),gadg::toLipschitzQuaternion(std::get<7>(cell))
-    );
+    IntegerRegion regionRounding(gadg::Region cell) {
+        return std::make_tuple(
+                gadg::toLipschitzQuaternion(std::get<0>(cell)), gadg::toLipschitzQuaternion(std::get<1>(cell)),
+                gadg::toLipschitzQuaternion(std::get<2>(cell)), gadg::toLipschitzQuaternion(std::get<3>(cell)),
+                gadg::toLipschitzQuaternion(std::get<4>(cell)), gadg::toLipschitzQuaternion(std::get<5>(cell)),
+                gadg::toLipschitzQuaternion(std::get<6>(cell)), gadg::toLipschitzQuaternion(std::get<7>(cell))
+        );
+    }
+
+// return a set of points inside a region with integer points
+    std::vector<QuaternionScalar<int>> intQuaternionInsideRegion(IntegerRegion integerArea){
+        
+    }
+
 }
-
-
